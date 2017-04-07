b687918f353ce3452609e83fd5a78f1849b4f993
xiong116276 <1162767656@qq.com>
2017-04-07 10:55:03 +0800

    my fisrt vue

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="test">
+    <h1>my first vue</h1>
+    <input type="text" v-model="msg" placeholder="changemsg">
+    <p>{{msg}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'test',
+    data () {
+      return {
+        msg: 'Hello Vue!'
+      }
+    }
+  }
+</script>
+
+<style>
+body{
+  font-family: "microsoft yahei";
+}
+</style>
